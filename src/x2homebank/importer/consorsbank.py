de6f66ad224ba78b20171cea2e32f3f60ba60a36
Sven Festersen <sven@sven-festersen.de>
2025-02-09 21:51:26 +0100

    Correct line skip value

--- src/x2homebank/importer/consorsbank.py
+++ src/x2homebank/importer/consorsbank.py
@@ -21,7 +21,7 @@ class ConsorsbankDialect(csv.Dialect):
 class ConsorsbankImporter(TransactionImporter):
     """Import transactions from CSV files in Consorsbank format."""
 
-    ignore_header_lines = 4
+    ignore_header_lines = 6
 
     def read(self, f: TextIO) -> TransactionList:
         date_reader = lambda x: datetime.strptime(x, "%d.%m.%Y")
